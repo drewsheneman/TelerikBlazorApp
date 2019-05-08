070d3b2e89344e0ed1d98a6e7123b9362af12917
Drew Sheneman <drewsheneman@gmail.com>
2019-05-08 08:32:04 -0400

    first commit

---
+++ TelerikBlazorApp.Shared/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace TelerikBlazorApp.Shared
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int UnitsInStock { get; set; }
+    }
+}
